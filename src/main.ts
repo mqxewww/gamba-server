d3e890232aa4c1a20d055eacfee902b3df5507bd
mqxewww <noizetmaxence@proton.me>
2025-01-28 13:05:30 +0100

    feat: add @nestjs/swagger for API documentation and route testing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "src/app.module";
+import pJson from "../package.json";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -8,6 +10,17 @@ async function bootstrap(): Promise<void> {
     credentials: true,
     origin: ["localhost:3000"]
   });
+
+  const swaggerDocument = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle(`Documentation for ${pJson.name}`)
+      .setVersion(pJson.version)
+      .setDescription(pJson.description)
+      .build()
+  );
+
+  SwaggerModule.setup("/", app, swaggerDocument);
 
   await app.listen(+process.env.API_PORT);
 }
