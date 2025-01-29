aaa05167fb26e1d3a3f2145cd9a184809f570a4d
mqxewww <noizetmaxence@proton.me>
2025-01-29 18:36:52 +0100

    chore: migrate from Bun to Node to resolve MikroORM issues, update dependencies

--- environment.d.ts
+++ environment.d.ts
@@ -1,10 +1,14 @@
-declare module "bun" {
-  interface Env {
-    API_PORT: number;
-    DATABASE_HOST: string;
-    DATABASE_PORT: number;
-    DATABASE_USERNAME: string;
-    DATABASE_PASSWORD: string;
-    DATABASE_NAME: string;
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      API_PORT: number;
+      DATABASE_HOST: string;
+      DATABASE_PORT: number;
+      DATABASE_USERNAME: string;
+      DATABASE_PASSWORD: string;
+      DATABASE_NAME: string;
+    }
   }
 }
+
+export {};
